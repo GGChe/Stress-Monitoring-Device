7d4138a3d44fd378e00b163edb7b5c9d2ac86666
GGChe <gabrielgaleote@gmail.com>
2019-03-10 23:22:34 +0000

    Modification of Buzzer class

---
+++ Code/Buzzer.cpp
@@ -0,0 +1,41 @@
+#include <Buzzer.h>
+
+/*
+The method Buzzer is the constructor of the class. It initializes the pin value to 0.
+
+*/
+
+void Buzzer::Buzzer() {
+
+	pin = 0;
+
+}
+
+
+void Buzzer::stressAlarm() {
+
+	cout << "StressAlarm" << endl;
+
+}
+
+void Buzzer::emergencyAlarm() {
+
+	cout << "emergencyAlarm" << endl;
+
+}
+
+void Buzzer::Beep() {
+
+	cout << "Beep" << endl;
+
+}
+
+
+
+
+
+
+
+
+
+
--- Code/Buzzer.h
+++ Code/Buzzer.h
@@ -4,26 +4,12 @@ using namespace std;
 class Buzzer {
 
 public:
-  void stressAlarm();
-  void emergencyAlarm();
-  void Beep();
+
+	int pin
+	void stressAlarm();
+	void emergencyAlarm();
+	void Beep();
 private:
+	void Buzzer();
 };
 
-void Buzzer::stressAlarm(){
-
-  cout<<"StressAlarm"<<endl;
-
-}
-
-void Buzzer::emergencyAlarm(){
-
-  cout<<"emergencyAlarm"<<endl;
-
-}
-
-void Buzzer::Beep(){
-
-  cout<<"Beep"<<endl;
-
-}
